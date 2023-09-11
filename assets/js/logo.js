1d6b87fee5b8be786828e0645dafa1ec18fb34ee
Zsolt Sz. Sztupák <mail@sztupy.hu>
2023-09-11 16:42:19 +0100

    Split projects onto separate page. Remove Twitter. Add global portfolio menu

---
+++ assets/js/logo.js
@@ -0,0 +1,17 @@
+(function(){
+var head = document.head;
+var link = document.createElement("link");
+link.type = "text/css";
+link.rel = "stylesheet";
+link.href = "https://sztupy.hu/assets/css/logo.css";
+head.appendChild(link);
+var body = document.body;
+var div = document.createElement("div");
+div.id = "zssr-logo";
+div.innerHTML = '<a href="https://zsolt.raven.scot/projects"></a>';
+if (body.firstChild) {
+  body.insertBefore(div, body.firstChild);
+} else {
+  body.appendChild(div);
+}
+})();
